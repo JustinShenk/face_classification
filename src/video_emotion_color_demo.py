16b7a4e16aafb19467ecaa4e525f3eabf79c8218
Justin Shenk <shenk.justin@gmail.com>
2017-12-06 13:18:58 -0800

    Put loop in try loop, flip image for mirroring, add emotions under face

--- src/video_emotion_color_demo.py
+++ src/video_emotion_color_demo.py
@@ -12,6 +12,7 @@ from utils.inference import apply_offsets
 from utils.inference import load_detection_model
 from utils.preprocessor import preprocess_input
 
+np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 # parameters for loading data and images
 detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
 emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
@@ -32,58 +33,75 @@ emotion_target_size = emotion_classifier.input_shape[1:3]
 emotion_window = []
 
 # starting video streaming
-cv2.namedWindow('window_frame')
+cv2.namedWindow('window_frame', cv2.WINDOW_NORMAL)
+
 video_capture = cv2.VideoCapture(0)
+video_capture.set(3, 600)
+video_capture.set(4, 800)
 while True:
-    bgr_image = video_capture.read()[1]
-    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
-    faces = detect_faces(face_detection, gray_image)
+    try:
+        bgr_image = video_capture.read()[1]
+        bgr_image = cv2.flip(bgr_image, 1)
+        gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+        rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+        faces = detect_faces(face_detection, gray_image)
 
-    for face_coordinates in faces:
+        for face_coordinates in faces:
 
-        x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-        gray_face = gray_image[y1:y2, x1:x2]
-        try:
-            gray_face = cv2.resize(gray_face, (emotion_target_size))
-        except:
-            continue
+            x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+            gray_face = gray_image[y1:y2, x1:x2]
+            try:
+                gray_face = cv2.resize(gray_face, (emotion_target_size))
+            except:
+                continue
 
-        gray_face = preprocess_input(gray_face, True)
-        gray_face = np.expand_dims(gray_face, 0)
-        gray_face = np.expand_dims(gray_face, -1)
-        emotion_prediction = emotion_classifier.predict(gray_face)
-        emotion_probability = np.max(emotion_prediction)
-        emotion_label_arg = np.argmax(emotion_prediction)
-        emotion_text = emotion_labels[emotion_label_arg]
-        emotion_window.append(emotion_text)
+            gray_face = preprocess_input(gray_face, True)
+            gray_face = np.expand_dims(gray_face, 0)
+            gray_face = np.expand_dims(gray_face, -1)
+            emotion_prediction = emotion_classifier.predict(gray_face)
+            emotion_probability = np.max(emotion_prediction)
+            emotion_label_arg = np.argmax(emotion_prediction)
+            emotion_text = emotion_labels[emotion_label_arg]
+            emotion_window.append(emotion_text)
 
-        if len(emotion_window) > frame_window:
-            emotion_window.pop(0)
-        try:
-            emotion_mode = mode(emotion_window)
-        except:
-            continue
+            if len(emotion_window) > frame_window:
+                emotion_window.pop(0)
+            try:
+                emotion_mode = mode(emotion_window)
+            except:
+                continue
 
-        if emotion_text == 'angry':
-            color = emotion_probability * np.asarray((255, 0, 0))
-        elif emotion_text == 'sad':
-            color = emotion_probability * np.asarray((0, 0, 255))
-        elif emotion_text == 'happy':
-            color = emotion_probability * np.asarray((255, 255, 0))
-        elif emotion_text == 'surprise':
-            color = emotion_probability * np.asarray((0, 255, 255))
-        else:
-            color = emotion_probability * np.asarray((0, 255, 0))
+            if emotion_text == 'angry':
+                color = emotion_probability * np.asarray((255, 0, 0))
+            elif emotion_text == 'sad':
+                color = emotion_probability * np.asarray((0, 0, 255))
+            elif emotion_text == 'happy':
+                color = emotion_probability * np.asarray((255, 255, 0))
+            elif emotion_text == 'surprise':
+                color = emotion_probability * np.asarray((0, 255, 255))
+            else:
+                color = emotion_probability * np.asarray((0, 255, 0))
 
-        color = color.astype(int)
-        color = color.tolist()
+            color = color.astype(int)
+            color = color.tolist()
 
-        draw_bounding_box(face_coordinates, rgb_image, color)
-        draw_text(face_coordinates, rgb_image, emotion_mode,
-                  color, 0, -45, 1, 1)
+            draw_bounding_box(face_coordinates, rgb_image, color)
+            draw_text(face_coordinates, rgb_image, emotion_mode,
+                      color, 0, -45, 1, 1)
+            for i in range(len(emotion_prediction[0])):
+                print(face_coordinates)
+                x, y, w, h = face_coordinates
+                emotion_text_coordinates = (x, y + h + 20 * i)
+                emotion_text = emotion_labels[i]
+                emotion_score = "{}: {:.2f}".format(
+                    emotion_text, emotion_prediction[0][i])
+                draw_text(emotion_text_coordinates, rgb_image, emotion_score,
+                          (0, 255, 0), 0, 45, 0.5, 1)
+    except:
+        pass
 
     bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    # bgr_image = cv2.flip(bgr_image, )
     cv2.imshow('window_frame', bgr_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
